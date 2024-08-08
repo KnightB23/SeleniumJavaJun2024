51d53eee6445c6837dcb1bba1315735844d61da6
KnightB23 <uneamie96@gmail.com>
2024-08-08 20:24:47 +0700

    Remove 1 line "KB edited" of TC1

--- src/TC1.java
+++ src/TC1.java
@@ -2,6 +2,5 @@ public class TC1 {
     public static void main(String[] args) {
         System.out.println("TC1");
         System.out.println("Edited 08/08/2024");
-        System.out.println("KB edited");
     }
 }
