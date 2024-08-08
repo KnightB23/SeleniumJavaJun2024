e5bf8a9a3db54f5b98269e21c2320e722f309a32
KnightB23 <uneamie96@gmail.com>
2024-08-08 20:22:33 +0700

    Update a new line for TC1

--- src/TC1.java
+++ src/TC1.java
@@ -2,5 +2,6 @@ public class TC1 {
     public static void main(String[] args) {
         System.out.println("TC1");
         System.out.println("Edited 08/08/2024");
+        System.out.println("KB edited");
     }
 }
